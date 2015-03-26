2fd26f10543b42af07851ac7b128a0df664d4a5f
Summa.Iru <summaIru@gmail.com>
2015-03-26 21:25:15 +0000

    disable voting button after voting

--- client/templates/books/book.js
+++ client/templates/books/book.js
@@ -1,21 +1,34 @@
+Template.book.helpers({
+    'has_voted': function(){
+          var lcb_cookie= "lcb-votes-" + this._id;  
+          if (!Session.get(lcb_cookie)) {             
+             // console.log (lcb_cookie + " not found");
+              return false;
+          } else {            
+             // console.log (lcb_cookie + " voted");
+              return true;
+         }
+     }
+}); 
 
 if (isMobile.any()) {
    Template.book.events({ 
      // focus event is needed to make this work on a mobile device.
        'focus .votes, click .votes': function() {
-        console.log("you focused! " + this._id);
-        Books.update({_id: this._id}, {$inc: {votes: 1}});
+           var lcb_cookie= "lcb-votes-" + this._id;   
+           Session.setPersistent(lcb_cookie, "voted");
+           Books.update({_id: this._id}, {$inc: {votes: 1}});
      } 
    });
 }
- else {
+ else {   
    Template.book.events({
        'click .votes': function() {
-        console.log("you clicked!" + this._id);
-        Books.update({_id: this._id}, {$inc: {votes: 1}});
+          var lcb_cookie= "lcb-votes-" + this._id;   
+          Session.setPersistent(lcb_cookie, "voted");            
+          Books.update({_id: this._id}, {$inc: {votes: 1}});
      }
    });              
-                     
 }
 
 
